3cb9d40f05793ab5fae4b21e528bcf25e3344155
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2024-02-23 21:23:47 +0600

    added

--- src/components/Ui/Banner.js
+++ src/components/Ui/Banner.js
@@ -15,11 +15,11 @@ const Banner = () => {
 
                      <p className="text-white text-[18px] md:mt-8 mt-6 font-normal leading-[28px]">I've spent the last 10 years in tech and have 4 years in elementary school tutoring. I'm also a proud graduate of Hampton University and a resident of Church Hill.</p>
 
-                     <button className="md:px-6 md:py-3 px-4 py-2 bg-white text-[#242424] md:text-[18px] text-[14px] font-bold leading-7 uppercase rounded-[8px] md:mt-8 mt-6 flex items-center gap-[10px] hover:bg-[#069A71] hover:border duration-300"><Image className="w-6 h-6" src={Calendar} alt="Calendar icon" /> Schedule Tutoring Session</button>
+                     <button className="md:px-6 md:py-3 px-4 py-2 bg-white text-[#242424] md:text-[18px] text-[14px] font-bold leading-7 uppercase rounded-[8px] md:mt-8 mt-6 flex items-center gap-[10px] hover:bg-[#F7EB0B] duration-300"><Image className="w-6 h-6" src={Calendar} alt="Calendar icon" /> Schedule Tutoring Session</button>
 
                 </div>
 
-                <div className="lg:w-[424px] w-[320px] lg:h-[548px] md:h-[748px] h-[438px] mx-auto lg:mx-0 mt-10 md:mt-16 lg:mt-0">
+                <div className="lg:w-[424px] w-[320px] md:w-[600px] lg:h-[548px] md:h-[748px] h-[438px] mx-auto lg:mx-0 mt-10 md:mt-16 lg:mt-0">
                     <Image className="w-full h-full " src={chad} alt="chad image " placeholder="blur"/>
                 </div>
             </div>
